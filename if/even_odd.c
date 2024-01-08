2b62bdde74d3f0a65563c1a87561a9bb0f7e71c0
srikanthpragada <srikanthpragada@gmail.com>
2024-01-08 17:54:42 +0530

    08-JAN

---
+++ if/even_odd.c
@@ -0,0 +1,19 @@
+// Take a number and display whether it is even or odd
+
+#include <stdio.h>
+
+void main()
+{
+ int a;
+
+    printf("Enter a number :");
+    scanf("%d",&a);
+
+    if (a % 2 == 0)
+        printf("Even");
+    else
+        printf("Odd");
+
+
+
+}
---
+++ if/max.c
@@ -0,0 +1,17 @@
+// Take two numbers and display the largest
+
+#include <stdio.h>
+
+void main()
+{
+ int a, b;
+
+    printf("Enter two numbers :");
+    scanf("%d%d",&a,&b);
+
+    if (a > b)
+        printf("%d ",a);
+    else
+        printf("%d ",b);
+
+}
